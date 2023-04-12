c5f7f3ceed3e7ffa0c592b4e9d1ed8f3732896cf
FLYBYME <mangoraft@gmail.com>
2023-04-12 14:30:26 +0000

    Added for sudo run

---
+++ server.js
@@ -0,0 +1,12 @@
+const { ServiceBroker } = require("moleculer");
+
+const config=require("./moleculer.config");
+
+
+const broker = new ServiceBroker(config);
+
+broker.loadServices("./services");
+
+broker.start().then(()=>{
+    broker.repl()
+})
